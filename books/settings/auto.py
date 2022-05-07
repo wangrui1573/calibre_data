fa994216b685734f558b8f98269f2ad2f2a1e482
wangrui1573 <real-wang@qq.com>
2022-05-08 07:53:40 +0800

    2020508

---
+++ books/settings/auto.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+#-*- coding: UTF-8 -*-
+
+import os
+settings = {
+'BOOK_NAMES_FORMAT'           : 'utf8',
+'INVITE_MODE'                 : False,
+'avatar_service'              : 'https://cravatar.cn',
+'cookie_secret'               : 'eed76e5c-ce11-11ec-b8f6-0242ac120004',
+'installed'                   : True,
+'settings_path'               : '/data/books/settings/',
+'site_title'                  : 'Real',
+}
