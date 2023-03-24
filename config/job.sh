221431349c7b39dd8847d58a1b8ee30520eea887
wangrui1573 <real-wang@qq.com>
2023-03-25 02:00:03 +0800

    cron auto commit

--- config/job.sh
+++ config/job.sh
@@ -1,6 +1,6 @@
 cd /docker/calibre/books
 sh /docker/calibre/books/addbook.sh
 sleep 3
-/usr/bin/python3 /docker/calibre/books/pic.py
+/usr/bin/python3.6 /docker/calibre/books/pic.py
 sleep 3 
-/usr/bin/python3 /docker/calibre/books/set_pic.py 
+/usr/bin/python3.6 /docker/calibre/books/set_pic.py 
