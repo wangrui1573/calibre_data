b3bbc0660bdd471ed893ae356a4a23a2af0b0723
wangrui1573 <real-wang@qq.com>
2023-09-14 02:00:02 +0800

    cron auto commit

--- autocommit.sh
+++ autocommit.sh
@@ -6,10 +6,10 @@ git commit -m "cron auto commit"
 git push origin tencent
 git status
 
-cd /docker/wallabag
-pwd
-git add . -A
-git commit -m "cron auto commit"
-git push origin main
-git status
+#cd /docker/wallabag
+#pwd
+#git add . -A
+#git commit -m "cron auto commit"
+#git push origin main
+#git status
 
